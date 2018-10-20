a00f488983182029f6d50b8dfd78759f1aebe2f4
SamuelVera <samuelvera1999@gmail.com>
2018-10-19 22:55:32 -0400

    Arreglo de las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes} from '@angular/router';
+
 import { NavbarComponent } from './login/navigation/navbar/navbar.component';
 import { HomeComponent } from './login/home/home.component';
 import { LoginComponent } from './login/login.component';
@@ -12,88 +13,43 @@ import { NavbarAdminComponent } from './login/navigation/navbar-admin/navbar-adm
 import { MenuAdminComponent } from './admin/menu-admin/menu-admin.component';
 import { HomeAdminComponent } from './admin/home-admin/home-admin.component';
 import { EnviosComponent } from './admin/envios/envios.component';
+import { EditarPlatoComponent } from './admin/menu-admin/editar-plato/editar-plato.component';
 
 const routes: Routes = [
-{
+
+  {
+    path: '',
+    children: [
+      { path: 'menu-admin/editar/:id', component: EditarPlatoComponent},
+      { path: 'menu-admin', component: MenuAdminComponent},
+      { path: 'home-admin', component: HomeAdminComponent},
+      { path: 'envios', component: EnviosComponent},
+    ],
+    component: NavbarAdminComponent,
+  },
+
+  {
+    path: '',
+    children: [
+      { path: 'home', component: HomeComponent},
+      { path: 'menu', component: MenuComponent},
+      { path: 'ayuda', component: HelpComponent},
+      { path: 'user', component: CuentaComponent},
+
+    ],
+    component: NavbarComponent,
+  },
+
+  {
     path: '',
     children: [
       { path: '', redirectTo: '/login', pathMatch: 'full'},
       { path:'login', component: LoginComponent},
+      { path: 'form', component: FormComponent},
     ],
     component: NavbarLoginComponent
-},
+  }
 
-{
-  path: '',
-  children: [
-    { path:'form', redirectTo: '/form', pathMatch: 'full'},
-    { path:'form', component: FormComponent},
-  ],
-  component: NavbarLoginComponent
-},
-
-{
-  path: '',
-  children: [
-    { path: 'home', redirectTo: '/home', pathMatch: 'full'},
-    { path: 'home', component: HomeComponent},
-  ],
-  component: NavbarComponent,
-},
-
-{
-  path: '',
-  children: [
-    { path: 'menu', redirectTo: '/menu', pathMatch: 'full'},
-    { path: 'menu', component: MenuComponent},
-  ],
-  component: NavbarComponent,
-},
-
-{
-  path: '',
-  children: [
-    { path: 'ayuda', redirectTo: '/ayuda', pathMatch: 'full'},
-    { path: 'ayuda', component: HelpComponent},
-  ],
-  component: NavbarComponent,
-},
-
-{
-  path: '',
-  children: [
-    { path: 'user', redirectTo: '/user', pathMatch: 'full'},
-    { path: 'user', component: CuentaComponent},
-  ],
-  component: NavbarComponent,
-},
-
-{
-  path: '',
-  children: [
-    { path: 'menu-admin', redirectTo: '/menu-admin', pathMatch: 'full'},
-    { path: 'menu-admin', component: MenuAdminComponent},
-  ],
-  component: NavbarAdminComponent,
-},
-
-{
-  path: '',
-  children: [
-    { path: 'home-admin', redirectTo: '/home-admin', pathMatch: 'full'},
-    { path: 'home-admin', component: HomeAdminComponent},
-  ],
-  component: NavbarAdminComponent,
-},
-
-{
-  path: '',
-  children: [
-    { path: 'envios', redirectTo: '/envios', pathMatch: 'full'},
-    { path: 'envios', component: EnviosComponent},
-  ],
-  component: NavbarAdminComponent,
-}
 ]
 
 @NgModule({
