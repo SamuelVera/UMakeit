e8d27c3a828622264364ec6ab989c6485de9fcea
SamuelVera <samuelvera1999@gmail.com>
2018-10-28 21:06:38 -0400

    Arreglo en las propiedades de los clientes

--- src/app/clases/cliente.ts
+++ src/app/clases/cliente.ts
@@ -5,16 +5,12 @@ export interface Cliente{
     email: string; //Nombre del usuario
     displayName ?: string; //Correo electrónico
     cedula ?: string; //Número de cédula
-    password ?: string; //Password
     telefono ?: string; //Número de teléfono
-    direcciones ?:[ //Direcciones del cliente
-        {
-            direccion: string;
-        }
-    ]
+    direccion ?: string;
     ordenes ?: [ //Arreglo de platos
         {
             plato: Plato;
         }
     ]
+    admin ?: boolean;
 }
